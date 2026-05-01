8bc8e6a86fce44690d2be4a5512d40ed9cb76234
Cody Lee <cody.lee@datadoghq.com>
2026-05-01 05:03:01 -0500

    Revert "Update pup to v0.56.0 (#64)" (#65)
    This reverts commit 42d29352ad8e0cb198e700c65bf9aef694bfd81e.

--- Formula/pup.rb
+++ Formula/pup.rb
@@ -6,26 +6,26 @@
 class Pup < Formula
   desc "Go-based command-line wrapper for easy interaction with Datadog APIs"
   homepage "https://github.com/datadog-labs/pup"
-  version "0.56.0"
+  version "0.54.1"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/datadog-labs/pup/releases/download/v0.56.0/pup_0.56.0_Darwin_arm64.tar.gz"
-      sha256 "35ae0b092184c94e7e9ce4ccd06c7f32a3d3cf3bb8dacd62f8af49e498621c87"
+      url "https://github.com/datadog-labs/pup/releases/download/v0.54.1/pup_0.54.1_Darwin_arm64.tar.gz"
+      sha256 "d94ca86a1d7b0e8521392242ba880de222d0054b38db05cbf016bf5852d1728a"
     else
-      url "https://github.com/datadog-labs/pup/releases/download/v0.56.0/pup_0.56.0_Darwin_x86_64.tar.gz"
-      sha256 "1aa13ca5d5fc4a774d509ed9037e67ab2216d8564174154df75e57a1cff8213c"
+      url "https://github.com/datadog-labs/pup/releases/download/v0.54.1/pup_0.54.1_Darwin_x86_64.tar.gz"
+      sha256 "e110fa3134491ddd499d0bb11012f277cb9e0f45120e28b70286d412b8e8fa83"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/datadog-labs/pup/releases/download/v0.56.0/pup_0.56.0_Linux_arm64.tar.gz"
-      sha256 "04b9d61902335bc7a79841be1dddc1dff7df42b388103f095f57907ea9296b4b"
+      url "https://github.com/datadog-labs/pup/releases/download/v0.54.1/pup_0.54.1_Linux_arm64.tar.gz"
+      sha256 "006df0e1418c3af85dd20ba56cd8c1b6e57729380cc666f7d1f9362e78bb2d62"
     else
-      url "https://github.com/datadog-labs/pup/releases/download/v0.56.0/pup_0.56.0_Linux_x86_64.tar.gz"
-      sha256 "0e098f7469ddd0585a1aeadf59c293ef2accceeae409b92f300a39a9372cd709"
+      url "https://github.com/datadog-labs/pup/releases/download/v0.54.1/pup_0.54.1_Linux_x86_64.tar.gz"
+      sha256 "c964fb0555b4c38f5eff41118f28e2236c2ae95586d275cf4cd6f3bffb59c968"
     end
   end
 
